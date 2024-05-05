386f3f0079eebe3f1f25fca8d385d8dccef02d0b
Tamerlan13101975 <166745761+Tamerlan13101975@users.noreply.github.com>
2024-05-05 22:12:43 +0300

    add new block

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+
+
 class Animal:
     def __init__(self, name, age):
         self.name = name
@@ -25,12 +27,29 @@ class ZooKeeper(Employee):
 
 
     def feed_animal(self, animal):
-        print(f"Зоосмотритель {self, name} кормит животное {animal, name}")
+        print(f"Зоосмотритель {self.name} кормит животное {animal.name}")
 
 
 class Veterinarian(Employee):
     def __init__(self, name):
         super().__init__(name, "Veterinarian")
+
+
+    def heal_animal(self, animal):
+        print(f"Ветеринар {self.name} лечит животное {animal.name}")
+
+
+    def save_zoo_info(zoo, filename):
+        with open(filename, "w") as f:
+            zoo_data = {"animals": [], "employees": []}
+            for animal in zoo.animals:
+                zoo_data["animals"].append({"name": animal.name, "age": animal.age})
+            for employee in zoo.employees:
+                zoo_data["employees"].append({"name": employee.name, "role": employee.role})
+            json.dump(zoo_data, f)
+
+
+
 
 
 
